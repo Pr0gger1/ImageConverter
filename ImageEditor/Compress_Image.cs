06e7d103346e9ac6371fe20cf4e9c9abb687e7aa
Artem <kraus2202200491@gmail.com>
2021-08-09 11:16:06 +0300

    .

--- ImageEditor/Compress_Image.cs
+++ ImageEditor/Compress_Image.cs
@@ -5,7 +5,39 @@ using System.IO;
 
 namespace ImageEditor
 {
-    public class Jpeg_Compression
+    //public class Jpeg_Compression
+    //{
+    //    protected static ImageCodecInfo GetEncoderInfo(string mime_type)
+    //    {
+    //        ImageCodecInfo[] encoders = ImageCodecInfo.GetImageEncoders();
+    //        for (int i = 0; i <= encoders.Length; i++)
+    //        {
+    //            if (encoders[i].MimeType == mime_type) return encoders[i];
+    //        }
+    //        return null;
+    //    }
+    //    public static void Compress(string filename, string saveToDir , int quality)
+    //    {
+    //        int com_ratio = 100 - quality;
+
+    //        using (Bitmap img = new Bitmap(filename))
+    //        {
+    //            EncoderParameters EncoderImage = new EncoderParameters()
+    //            {
+    //                Param = new EncoderParameter[]
+    //                {
+    //                    new EncoderParameter(Encoder.Quality, 100L - com_ratio)
+    //                }
+    //            };
+    //            img.Save(
+    //                Path.ChangeExtension(saveToDir, "").Trim('.') + $"_compressed.jpg",
+    //                ImageCodecInfo.GetImageEncoders()[1],
+    //                EncoderImage);
+    //        }
+    //    }
+    //}
+
+    public class ImgCompression
     {
         protected static ImageCodecInfo GetEncoderInfo(string mime_type)
         {
@@ -16,29 +48,7 @@ namespace ImageEditor
             }
             return null;
         }
-        public static void Compress(string filename, string saveToDir , int quality)
-        {
-            int com_ratio = 100 - quality;
 
-            using (Bitmap img = new Bitmap(filename))
-            {
-                EncoderParameters EncoderImage = new EncoderParameters()
-                {
-                    Param = new EncoderParameter[]
-                    {
-                        new EncoderParameter(Encoder.Quality, 100L - com_ratio)
-                    }
-                };
-                img.Save(
-                    Path.ChangeExtension(saveToDir, "").Trim('.') + $"_compressed.jpg",
-                    ImageCodecInfo.GetImageEncoders()[1],
-                    EncoderImage);
-            }
-        }
-    }
-
-    public class DirJpegCompression : Jpeg_Compression
-    {
         public static Bitmap LoadBitmap(string file_name)
         {
             Bitmap result;
@@ -71,21 +81,5 @@ namespace ImageEditor
 
             }
         }
-        //public static void CompressDirJpg(Image img, string save_to_dir, int quality)
-        //{
-        //    int com_ratio = 100 - quality;
-        //    try
-        //    {
-        //        EncoderParameters encoder_params = new EncoderParameters(1);
-        //        encoder_params.Param[0] = new EncoderParameter(Encoder.Quality, 100L - com_ratio);
-        //        ImageCodecInfo img_codec_info = GetEncoderInfo("image/jpeg");
-        //        img.Save(save_to_dir, img_codec_info, encoder_params);
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw ex;
-        //    }
-        //}
     }
 }
